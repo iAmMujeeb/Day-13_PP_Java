261f5270c158d1ec57a94d25b96e6ccc1c8995e2
Your Name <saifi.mujeeb2@gmail.com>
2023-04-27 15:27:49 +0530

    Added: to find max of three floats

--- src/max/MaxGenMain.java
+++ src/max/MaxGenMain.java
@@ -5,13 +5,22 @@ public class MaxGenMain {
         Integer num1 = 10;
         Integer num2 = 20;
         Integer num3 = 30;
-        Integer max = num1;
+        Integer maxInt = num1;
+        Float num4 = 10.5F;
+        Float num5 = 20.5F;
+        Float num6 = 30.5F;
+        Float maxFloat = num4;
 
-        if (num2.compareTo(max)>0)
-            max = num2;
+        if (num2.compareTo(maxInt)>0)
+            maxInt = num2;
         if (num3.compareTo(num2)>0)
-            max = num3;
+            maxInt = num3;
+        System.out.println("Maximum Integer Number = "+maxInt);
 
-        System.out.println("Maximum Number = "+max);
+        if (num5.compareTo(maxFloat)>0)
+            maxFloat = num5;
+        if (num6.compareTo(num5)>0)
+            maxFloat = num6;
+        System.out.println("Maximum Float Number = "+maxFloat);
     }
 }
